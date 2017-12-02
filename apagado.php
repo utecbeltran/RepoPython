8b380b1a3726c9dbe714d80f14ed107b089d1ca6
utecbeltran <33576240+utecbeltran@users.noreply.github.com>
2017-12-02 01:04:29 -0500

    Add files via upload
    Controlador pagina web

---
+++ apagado.php
@@ -0,0 +1,9 @@
+<?php
+
+// Cambiar el mensaje por la instruccion
+$msg = "apaga luz";
+
+//Enviar e-mail
+mail("raspberrypi3utec@gmail.com", "UCCI Apagado", $msg)
+
+?>
---
+++ prendido.php
@@ -0,0 +1,9 @@
+<?php
+
+// Cambiar el mensaje por la instruccion
+$msg = "prende luz";
+
+//Enviar e-mail
+mail("raspberrypi3utec@gmail.com", "UCCI Prende Luz", $msg)
+
+?>
